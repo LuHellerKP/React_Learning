03b17bbc4d839be4c850a653e2ab05893a8be3cd
Luiza Heller Kroeff Plá <luiza.heller@edu.pucrs.br>
2023-12-08 11:01:40 -0300

    Restarting
    Deleting and going againg.

--- reactrouter/src/routes/destroy.jsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "react-router-dom";
-import { deleteContact } from "../contacts";
-
-export async function action({ params }) {
-  //Contextual Errors:
-  throw new Error("oh dang!");
-  await deleteContact(params.contactId);
-  return redirect("/");
-}
