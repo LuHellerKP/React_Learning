3759292d72a3bb6caf7c57fa64aaf8fa2f965b28
Luiza Heller Kroeff Plá <luiza.heller@edu.pucrs.br>
2023-12-15 11:53:45 -0300

    Going again

--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -3,13 +3,24 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root, { loader as rootLoader } from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     // This is the first route -> root route -> the rest of the routes will render inside of it.
     // It server as the root layout of the UI.
     path: "/",
-    element: <div>Hello World!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
--- reactrouter/src/routes/root.jsx
+++ reactrouter/src/routes/root.jsx
@@ -1,5 +1,9 @@
 //Global layout for the app (The Root Route)
+import { Outlet, Link, useLoaderData } from "react-router-dom";
+import { getContacts } from "../contacts";
+
 export default function Root() {
+  const { contacts } = useLoaderData();
   return (
     <>
       <div id="sidebar">
@@ -13,7 +17,7 @@ export default function Root() {
               type="search"
               name="q"
             />
-            <div id="search-spinner" aria-hidden hidden={true}/>
+            <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
           <form method="post">
@@ -21,15 +25,32 @@ export default function Root() {
           </form>
         </div>
         <nav>
+          {contacts.length ? (
             <ul>
-                <li>
-                    <a href={`/contacts/1`}>Your Name</a>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
                 </li>
-                <li>
-                    <a href={`/contacts/2`}>Your Friend</a>
-                </li>
+              ))}
             </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
       </div>
     </>
   );
